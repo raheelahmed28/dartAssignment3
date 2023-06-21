40dca099a0a8f58be059b163ccda22f9e8ae48a4
raheel ahmed <raheelahmed7890@gmail.com>
2023-06-21 16:18:06 +0500

    na

---
+++ 10.dart
@@ -0,0 +1,22 @@
+void main(){
+Map<String,double> expenses={
+  'sun': 3000.0,
+  'mon': 3000.0,
+  'tue': 3234.0,
+  
+
+
+
+};
+if (expenses.containsValue('fri')){
+  
+}
+  else {
+
+
+    expenses['fri']=5000.0;
+  }
+
+print(expenses);
+
+}
