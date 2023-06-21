c684ebf596c1895cd770efbfebdfecadd526841c
raheel ahmed <raheelahmed7890@gmail.com>
2023-06-21 15:07:55 +0500

    working

---
+++ 5.dart
@@ -0,0 +1,14 @@
+// void main(){
+// var num1=[23,30,48,50];
+// List <bool> num2=[];
+// for (num1=0,num1>=num1 ,num++);
+// if (num1%==2){
+
+// ptint(add.)
+
+// }
+
+
+// }
+
+
---
+++ 9.dart
@@ -0,0 +1,15 @@
+void main(){
+Map<String , Map<String,String>> world=
+  {
+      "pakistan" : {
+'capital' :   "islamabad",
+'currency' : 'Pakistani Rupee',
+'Biggest city' : 'karachi',
+   
+      },
+
+
+  };
+  print(world.containsKey("pakistan"));
+}
+
