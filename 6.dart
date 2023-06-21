94a838a7722ec398cb231c32e45a26a977e0204c
raheel ahmed <raheelahmed7890@gmail.com>
2023-06-22 00:23:18 +0500

    na

--- 6.dart
+++ 6.dart
@@ -5,9 +5,6 @@ List<int> num1=[13,4,5,6,7,5];
 for(int index=0 ; index < num1.length ; index++){
 int num=num1[index];
 print(num*index);
-
-
-
 }
 
 
@@ -15,3 +12,4 @@ print(num*index);
 
 
 }
+///
