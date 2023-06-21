9930abdbbb44d68a91371a966e4ca03a2d9ee8f5
raheel ahmed <raheelahmed7890@gmail.com>
2023-06-21 13:45:38 +0500

    done 7

--- 7.dart
+++ 7.dart
@@ -2,9 +2,9 @@ import 'dart:math';
 
 void main(){
 List<int> Numbers=[24,54,65,99,18];
-print(Numbers.reduce((max)));
-print(Numbers.reduce((min)));
-
+int maxi=(Numbers.reduce(max));
+int mini=(Numbers.reduce(min));
+print("here $maxi is biggest and $mini is smallest");
 
 
 }
