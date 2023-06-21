1f7a62d9af13155a692905db04584030d9ad4809
raheel ahmed <raheelahmed7890@gmail.com>
2023-06-21 15:12:18 +0500

    na

---
+++ 11.dart
@@ -0,0 +1,26 @@
+
+void main(){
+var num1=[23,30,48,50];
+List <bool> num2=[];
+for (int Number in num1){
+if (Number%2==0){
+      num2.add(true);
+}
+else {
+
+    num2.add(false);
+
+}
+ 
+   
+
+}
+print(num2.remove(false));
+
+
+ print(num2);
+ 
+}
+
+
+
